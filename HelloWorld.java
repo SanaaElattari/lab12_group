1dd54af65516b9137ed226b66dc37cf8f11c96cc
SanaaElattari <sanaaelattari2006@gmail.com>
2025-11-03 14:56:27 -0500

    sanaa

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,1 +1,6 @@
-System.out.println("Hello World");
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World" );
+        System.out.println("This is Sanaa");
+    }
+}
